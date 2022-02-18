28208ba7927a272cc3d8d757634997353b63ef16
Michelle Randolph <danceofdisillusion@gmail.com>
2022-02-18 15:18:41 -0700

    Completed Excercises

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -5,12 +5,14 @@
 
 # Example: Write code that prints your name to the terminal:
 p "Alan Turing"
-
+puts "Michelle"
 # Write code that prints `Welcome to Turing!` to the terminal:
 p #YOUR CODE HERE
-
+puts "Welcome to Turing!"
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
 # YOUR CODE HERE
-
+bottles = 99
+puts "#{bottles} bottes of pop on the wall..."
 # Write out code to print one line from your favorite song or movie.
 # YOUR CODE HERE
+puts "line from a movie"
--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -5,27 +5,30 @@
 
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
-name = "Harry Potter"
+name = "Michelle Randolph"
 p name
 
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
 # YOUR CODE HERE
-
+p house_elf
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
 # YOUR CODE HERE
-
+hogwarts = "Harry Potter must not return to Hogwarts!"
+p hogwarts
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
 # YOUR CODE HERE
+students = students + 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
+students = students - 2
 p students
 
 
@@ -33,9 +36,15 @@ p students
 # Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+first_name = "Michelle"
+is_hungry = true
+number_of_pets = 0
+p first_name
+p is_hungry
+p number_of_pets
 # IN WORDS:
 # How did you decide to use the data type you did for each of the three variables above? 
+# a name should be a string. Is hungry implies the question of are you hungary or not so should be boolean nubmer of pets should be a number
 
 # Explain.
 
@@ -44,14 +53,20 @@ p students
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
-
+first_name = "V"
+is_hungry = false
+number_of_pets = 1
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
-
+p healthy_snacks + junk_food_snacks
+p "Number of total snacks #{healthy_snacks+junk_food_snacks}"
 #-------------------
 # FINAL CHECK
 #-------------------
